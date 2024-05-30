182e7240314fed7357ff91bf390645a9b31e3ddf
Ngô Đăng Trường Sa <Ngotruongsa2002@gmail.com>
2024-05-31 00:22:44 +0700

    Thêm Header vào đầu trang

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import Signup from '../components/Signup/Signup'
+import Signup from '../components/Signup/Signup';
+import Header from '../components/header/Header';
 function SignupPage(props) {
     return (
         <div>
+            <Header></Header>
             <Signup></Signup>
         </div>
     );
