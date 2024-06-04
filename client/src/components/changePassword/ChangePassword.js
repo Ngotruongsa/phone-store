31d7d9b596ab6daa73bd7bb3b6fa1a17eb9a2bd2
Ngô Đăng Trường Sa <Ngotruongsa2002@gmail.com>
2024-06-04 22:38:18 +0700

    Update hide/show password cho trang đổi mật khẩu

--- client/src/components/changePassword/ChangePassword.js
+++ client/src/components/changePassword/ChangePassword.js
@@ -4,10 +4,16 @@ import { useForm } from "react-hook-form";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import { changePassword } from "../../actions/UserAction"; // Cần tạo action này
+import { InputAdornment, TextField, IconButton } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import showPwdImg from '../../assets/svg/show-password.svg';
+import hidePwdImg from '../../assets/svg/hide-password.svg';
 
 function ChangePassword(prop) {
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
+  const [showOldPassword, setShowOldPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
   const dispatch = useDispatch();
   const history = useHistory();
   const {
@@ -27,6 +33,14 @@ function ChangePassword(prop) {
     reset();
   };
 
+  const handleClickShowOldPassword = () => {
+    setShowOldPassword(!showOldPassword);
+  };
+
+  const handleClickShowNewPassword = () => {
+    setShowNewPassword(!showNewPassword);
+  };
+
   useEffect(() => {
     if (!userInfo) {
       history.push("/");
@@ -40,29 +54,48 @@ function ChangePassword(prop) {
         Quản lý thông tin hồ sơ để bảo mật tài khoản
       </div>
       <form onSubmit={handleSubmit(onSubmit)} classname="form-changepassword">
+        <div className="input-container">
         <input
           {...register("oldPassword", { required: "Mật khẩu cũ là bắt buộc" })}
           placeholder="Mật khẩu cũ"
-          type="password"
+          type={showOldPassword ? "text" : "password"}
           value={oldPassword}
           onChange={(e) => setOldPassword(e.target.value)}
           className={errors.newPassword ? "input-error" : ""}
           required
         />
+        <img
+          title={showOldPassword ? <VisibilityOff /> : <Visibility />}
+          src={showOldPassword ? hidePwdImg : showPwdImg}
+          onClick={handleClickShowOldPassword}
+        />
+        </div>
         {errors.oldPassword && (
           <span className="error-message">{errors.oldPassword.message}</span>
         )}
         <div>
 
         </div>
+        
         <input
           {...register("newPassword", { required: "Mật khẩu mới là bắt buộc" })}
           placeholder="Mật khẩu mới"
-          type="password"
+          type={showNewPassword ? "text" : "password"}
           value={newPassword}
           onChange={(e) => setNewPassword(e.target.value)}
+          style={{position: "relative"}}
           className={errors.newPassword ? "input-error" : ""}
           required
+        />
+        <img
+          title={showNewPassword ? <VisibilityOff /> : <Visibility />}
+          src={showNewPassword ? hidePwdImg : showPwdImg}
+          onClick={handleClickShowNewPassword}
+          style={{  cursor: "pointer",
+            position: "absolute",
+            width: "20px",
+            right: "40px",
+            top: "280px",}}
         />
         {errors.newPassword && (
           <span className="error-message">{errors.newPassword.message}</span>
