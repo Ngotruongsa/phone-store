2a5c210433c5fd9701fb4a93bb38efeafd64a3ac
Ngô Đăng Trường Sa <Ngotruongsa2002@gmail.com>
2024-07-03 23:16:02 +0700

    Thêm chức năng Thêm sản phẩm vào giỏ hàng khi nhấn nút Mua Ngay ở trang Sản phẩm

--- client/src/components/allProduct/Product.js
+++ client/src/components/allProduct/Product.js
@@ -1,33 +1,55 @@
-import React from 'react';
-import {formatPrice} from '../../untils/index'
-
+import React from "react";
+import { useDispatch } from "react-redux";
+import { AddToCart } from "../../actions/CartAction";
+import { Link } from "react-router-dom";
+import { formatPrice } from "../../untils/index";
+import { message } from "antd";
 
 function Product(props) {
-    const { product } = props;
+  const dispatch = useDispatch();
+  const { product } = props;
 
-    function AddToCart(product) {
-        // const action = AddProduct(product);
-        // dispatch(action);
-    }
+  const success = () => {
+    message.success({
+      content: "Thêm vào giỏ hàng thành công",
+      duration: 1,
+      className: "custom-class",
+      style: {
+        position: "absolute",
+        right: "2rem",
+        top: "2rem",
+        margin: "1rem 0",
+      },
+    });
+  };
 
-    return (
-        <div className="hotsale-listproduct-product">
-            <a href={"/detail/" + product._id}>
-                <img src={product.image}></img>
-                <p className="hotsale-listproduct-product-name">{product.name}</p>
-                <div className="price">
-                    <span className="price1">{formatPrice(product.salePrice)}đ</span>
-                    <span className="price2">{formatPrice(product.price)}đ</span>
-                </div>
-            </a>
-            <div className="discount">
-                <p>{product.percentDiscount}%</p>
-            </div>
-            <div className="buy">
-                <a href="/cart" onClick={() => AddToCart(product)}> Mua Ngay</a>
-            </div>
+  function handleAddToCart(product) {
+    const action = AddToCart(product);
+    dispatch(action);
+    success()
+  }
+
+  return (
+    <div className="hotsale-listproduct-product">
+      <a href={"/detail/" + product._id}>
+        <img src={product.image}></img>
+        <p className="hotsale-listproduct-product-name">{product.name}</p>
+        <div className="price">
+          <span className="price1">{formatPrice(product.salePrice)}đ</span>
+          <span className="price2">{formatPrice(product.price)}đ</span>
         </div>
-    );
+      </a>
+      <div className="discount">
+        <p>{product.percentDiscount}%</p>
+      </div>
+      <div className="buy">
+        <Link to="/product" onClick={(e) => handleAddToCart(product)}>
+          {" "}
+          Mua Ngay
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
