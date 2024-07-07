62d6d4ee466ce1471916a41e092ef4f74a0209df
Ngô Đăng Trường Sa <Ngotruongsa2002@gmail.com>
2024-07-07 20:21:40 +0700

    Thêm phân trang (pagination) cho trang Sản phẩm

--- client/src/components/allProduct/AllProduct.js
+++ client/src/components/allProduct/AllProduct.js
@@ -1,40 +1,61 @@
-import React, { useEffect } from 'react';
-import ListProduct from './ListProduct'
-import './Sale.css'
-import {handlePercentDiscount} from '../../untils/index'
+import React, { useEffect, useState } from 'react';
+import ListProduct from './ListProduct';
+import './Sale.css';
+import { handlePercentDiscount } from '../../untils/index';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAllProduct} from '../../actions/ProductAction';
-
+import { getAllProduct } from '../../actions/ProductAction';
 import FilterProduct from './FilterProduct';
 import SortByPrice from './SortByPrice/SortByPrice';
-
+import { Pagination } from 'antd';
 
 function AllProduct(props) {
-    const dispatch = useDispatch()
-    
-    const product = useSelector(state => state.allProduct.product)
+    const dispatch = useDispatch();
+    const product = useSelector(state => state.allProduct.product);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [productsPerPage] = useState(20);
 
     useEffect(() => {
-        dispatch(getAllProduct())
+        dispatch(getAllProduct());
 
         return () => {
-            return []
-        }
-    }, [dispatch])
+            return [];
+        };
+    }, [dispatch]);
+
+    // Calculate current products
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = product ? product.slice(indexOfFirstProduct, indexOfLastProduct) : [];
+
+    // Change page
+    const paginate = pageNumber => {
+        setCurrentPage(pageNumber);
+        window.scrollTo(0, 0); // Scroll to top of the page
+    };
 
     return (
         <section id="hotsale iphone">
             <div className="hotsale">
-                <FilterProduct></FilterProduct>
-                <SortByPrice></SortByPrice>
-                {
-                   product && product.length > 0 ? (<ListProduct HotSaleProducts={handlePercentDiscount(product)}></ListProduct>) : (<span>Không có sản phẩm</span>)
-                }
+                <FilterProduct />
+                <SortByPrice />
+                {product && product.length > 0 ? (
+                    <>
+                        <ListProduct HotSaleProducts={handlePercentDiscount(currentProducts)} />
+                        <Pagination
+                            current={currentPage}
+                            pageSize={productsPerPage}
+                            total={product.length}
+                            onChange={paginate}
+                            style={{ textAlign: 'center', marginTop: '20px' }}
+                        />
+                    </>
+                ) : (
+                    <span>Không có sản phẩm</span>
+                )}
             </div>
         </section>
-
     );
 }
-
 
 export default AllProduct;
