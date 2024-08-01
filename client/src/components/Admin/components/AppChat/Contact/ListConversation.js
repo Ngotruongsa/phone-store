99e6e64b4e24d6e2b17a24e16606288bbef70bde
Ngô Đăng Trường Sa <Ngotruongsa2002@gmail.com>
2024-08-01 23:49:10 +0700

    Thêm chức năng tìm kiếm ở Chat

--- client/src/components/Admin/components/AppChat/Contact/ListConversation.js
+++ client/src/components/Admin/components/AppChat/Contact/ListConversation.js
@@ -1,23 +1,55 @@
-import React from "react";
-import {getFirstCharacterUser} from '../../../../../untils/index'
+import React, { useState } from "react";
+import { getFirstCharacterUser } from "../../../../../untils/index";
+import SearchIcon from '@mui/icons-material/Search';
 
 function ListConversation(props) {
   const { conversationList, onConversationClick } = props;
-  
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredConversations = conversationList.filter((conversation) =>
+    conversation.nameConversation
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="contact-list">
-      {conversationList.map((conversation) => (
-        <div
-          className="contact-list-item"
-          onClick={() => onConversationClick(conversation)}
-        >
-          <div className="contact-list-item-avarta">{getFirstCharacterUser(conversation.nameConversation)}</div>
-          <div className="contact-list-item-content">
-            <p className="contact-list-item-name">{conversation.nameConversation}</p>
-            <span className="contact-list-item-lastmessage"> {conversation.lastMessage} </span>
+    <div className="contact-list-container">
+      <div className="search-input-container">
+        <SearchIcon className="search-icon" />
+        <input
+          type="text"
+          placeholder="Search conversations..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className="contact-list-search"
+        />
+      </div>
+      <div className="contact-list">
+        {filteredConversations.map((conversation) => (
+          <div
+            key={conversation.id}
+            className="contact-list-item"
+            onClick={() => onConversationClick(conversation)}
+          >
+            <div className="contact-list-item-avarta">
+              {getFirstCharacterUser(conversation.nameConversation)}
+            </div>
+            <div className="contact-list-item-content">
+              <p className="contact-list-item-name">
+                {conversation.nameConversation}
+              </p>
+              <span className="contact-list-item-lastmessage">
+                {" "}
+                {conversation.lastMessage}{" "}
+              </span>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
